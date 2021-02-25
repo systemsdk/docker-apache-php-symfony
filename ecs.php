52f0e20c3443fcc4f8242d7badc5e57a6c9fb034
DKravtsov <dmitriy.kravtsov@systemsdk.com>
2021-02-25 03:07:59 +0200

    updated ecs

--- ecs.php
+++ ecs.php
@@ -30,8 +30,7 @@ use Symplify\CodingStandard\Fixer\Strict\BlankLineAfterStrictTypesFixer;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $imports = [
         '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/psr12.php',
-        '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/php70.php',
-        '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/php71.php',
+        '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/php80-migration-risky.php',
         '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/clean-code.php',
         '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/common.php',
         '/tools/03_ecs/vendor/symplify/easy-coding-standard/config/set/symfony.php',
